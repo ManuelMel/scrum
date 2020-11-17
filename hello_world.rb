3b4229f5da9c937feac586aba643bf432b03a120
ManuelMel <mmelendez.nonno@gmail.com>
2020-11-17 18:32:54 +0000

    Inicial

---
+++ hello_world.rb
@@ -0,0 +1,18 @@
+class KleerBot
+    
+    def initialize(idioma)
+        @idioma = idioma
+    end
+
+    def saludar(nombre)
+        if(@idioma == "EN")
+            return "Hello #{nombre}!"
+        else
+            return "Hola #{nombre}!" 
+        end
+    end
+
+end
+
+kleer_bot = KleerBot.new(ARGV[1])
+puts kleer_bot.saludar(ARGV[0])
