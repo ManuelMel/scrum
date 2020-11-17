55a554a0ddfd4df6390ee4ff1ffeb15039a6764f
ManuelMel <mmelendez.nonno@gmail.com>
2020-11-17 23:45:12 +0000

    Ahora valido que ingresen todos los parámetros requeridos

--- desafio1.rb
+++ desafio1.rb
@@ -7,50 +7,54 @@ class Desafio1
     end
     
     def mostrarDatos()
-        subTotal = @cant.to_f*@precio.to_f
-        vat = 0
-        desc = 0
+        if(@cant.nil? || @precio.nil? || @estado.nil?)
+            return "\n¡Ingresa todos los parámetros requeridos!"+ 
+            "\n//ruby desafio1.rb <cantidad> <precio unitario> <estado>//"+
+            "\n\n"
+        else
+            subTotal = @cant.to_f*@precio.to_f
+            vat = 0
+            desc = 0
 
-        if(@estado == "UT")
-            vat = 6.85
-        elsif(@estado == "NV")
-            vat = 8
-        elsif(@estado == "TX")
-            vat = 6.25
-        elsif(@estado == "AL")
-            vat = 4
-        elsif(@estado == "CA")
-            vat = 8.25
+            if(@estado == "UT")
+                vat = 6.85
+            elsif(@estado == "NV")
+                vat = 8
+            elsif(@estado == "TX")
+                vat = 6.25
+            elsif(@estado == "AL")
+                vat = 4
+            elsif(@estado == "CA")
+                vat = 8.25
+            end
+
+            if(subTotal>1000.0 && subTotal<= 5000.0)
+                desc = 3
+            elsif(subTotal>5000.0 && subTotal<= 7000.0)
+                desc = 5
+            elsif(subTotal>7000.0 && subTotal<= 10000.0)
+                desc = 7
+            elsif(subTotal>10000.0 && subTotal<= 50000.0)
+                desc = 10
+            elsif(subTotal>50000.0)
+                desc = 15
+            end
+
+            descValue = desc*subTotal/100
+            subTotal -= descValue
+            vatValue = vat*subTotal/100
+            total = subTotal+vatValue
+
+            return "\nLa cantidad ingresada es: ##{@cant}"+
+            "\nEl precio unitario ingresado es: $#{@precio}"+
+            "\nEl estado ingresado es: #{@estado}"+
+            "\n\nSubtotal: $#{subTotal}"+
+            "\nDescuento(#{desc}%): $#{descValue}"+
+            "\nVAT(#{vat}%): $#{vatValue}"+
+            "\n\nTotal: $#{total}"+
+            "\n\n"
         end
-
-        if(subTotal>1000.0 && subTotal<= 5000.0)
-            desc = 3
-        elsif(subTotal>5000.0 && subTotal<= 7000.0)
-            desc = 5
-        elsif(subTotal>7000.0 && subTotal<= 10000.0)
-            desc = 7
-        elsif(subTotal>10000.0 && subTotal<= 50000.0)
-            desc = 10
-        elsif(subTotal>50000.0)
-            desc = 15
-        end
-
-        descValue = desc*subTotal/100
-        subTotal -= descValue
-        vatValue = vat*subTotal/100
-        total = subTotal+vatValue
-
-
-        return "\nLa cantidad ingresada es: ##{@cant}"+
-        "\nEl precio unitario ingresado es: $#{@precio}"+
-        "\nEl estado ingresado es: #{@estado}"+
-        "\n\nSubtotal: $#{subTotal}"+
-        "\nDescuento(#{desc}%): $#{descValue}"+
-        "\nVAT(#{vat}%): $#{vatValue}"+
-        "\n\nTotal: $#{total}"+
-        "\n\n"
     end
-
 end
 
 kleer_bot = Desafio1.new(ARGV[0],ARGV[1],ARGV[2])
