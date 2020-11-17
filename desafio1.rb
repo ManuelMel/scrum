9c38954eb1639dab72185da518170dce6e4bb1f6
ManuelMel <mmelendez.nonno@gmail.com>
2020-11-17 23:49:02 +0000

    Ahora valido que ingresen un estado válido

--- desafio1.rb
+++ desafio1.rb
@@ -26,33 +26,40 @@ class Desafio1
                 vat = 4
             elsif(@estado == "CA")
                 vat = 8.25
+            else
+                vat = "X"
             end
 
-            if(subTotal>1000.0 && subTotal<= 5000.0)
-                desc = 3
-            elsif(subTotal>5000.0 && subTotal<= 7000.0)
-                desc = 5
-            elsif(subTotal>7000.0 && subTotal<= 10000.0)
-                desc = 7
-            elsif(subTotal>10000.0 && subTotal<= 50000.0)
-                desc = 10
-            elsif(subTotal>50000.0)
-                desc = 15
+            if(vat == "X")
+                return "¡#{@estado} no es un estado válido, ingresa UT, NV, TX, AL ó CA!"
+            else
+
+                if(subTotal>1000.0 && subTotal<= 5000.0)
+                    desc = 3
+                elsif(subTotal>5000.0 && subTotal<= 7000.0)
+                    desc = 5
+                elsif(subTotal>7000.0 && subTotal<= 10000.0)
+                    desc = 7
+                elsif(subTotal>10000.0 && subTotal<= 50000.0)
+                    desc = 10
+                elsif(subTotal>50000.0)
+                    desc = 15
+                end
+
+                descValue = desc*subTotal/100
+                subTotal -= descValue
+                vatValue = vat*subTotal/100
+                total = subTotal+vatValue
+
+                return "\nLa cantidad ingresada es: ##{@cant}"+
+                "\nEl precio unitario ingresado es: $#{@precio}"+
+                "\nEl estado ingresado es: #{@estado}"+
+                "\n\nSubtotal: $#{subTotal}"+
+                "\nDescuento(#{desc}%): $#{descValue}"+
+                "\nVAT(#{vat}%): $#{vatValue}"+
+                "\n\nTotal: $#{total}"+
+                "\n\n"
             end
-
-            descValue = desc*subTotal/100
-            subTotal -= descValue
-            vatValue = vat*subTotal/100
-            total = subTotal+vatValue
-
-            return "\nLa cantidad ingresada es: ##{@cant}"+
-            "\nEl precio unitario ingresado es: $#{@precio}"+
-            "\nEl estado ingresado es: #{@estado}"+
-            "\n\nSubtotal: $#{subTotal}"+
-            "\nDescuento(#{desc}%): $#{descValue}"+
-            "\nVAT(#{vat}%): $#{vatValue}"+
-            "\n\nTotal: $#{total}"+
-            "\n\n"
         end
     end
 end
