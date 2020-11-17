5d8989982776dfcb062d22a59cca16a55ef069da
ManuelMel <mmelendez.nonno@gmail.com>
2020-11-17 21:54:00 +0000

    Pedir cantidad, precio y mostrarlos al usuario

--- desafio1.rb
+++ desafio1.rb
@@ -1,10 +1,17 @@
 class Desafio1
+
+    def initialize(cantidad,precio)
+        @cant = cantidad
+        @precio = precio
+    end
     
-    def mostrarDatos(cantidad)
-        return cantidad
+    def mostrarDatos()
+        
+        return "La cantidad ingresada es: ##{@cant}"+
+        "\nEl precio unitario ingresado es: $#{@precio}"
     end
 
 end
 
-kleer_bot = Desafio1.new()
-puts kleer_bot.mostrarDatos(ARGV[0])
+kleer_bot = Desafio1.new(ARGV[0],ARGV[1])
+puts kleer_bot.mostrarDatos()
